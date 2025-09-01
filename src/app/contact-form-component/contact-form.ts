6b110756646dd1b1d78ce6ced8bda6be7778c719
MR.RODVELS <rodvels2002@outlook.com>
2025-09-01 00:11:14 -0500

    feat(contact-form.ts): Consumir el API create

--- src/app/contact-form-component/contact-form.ts
+++ src/app/contact-form-component/contact-form.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { ContactServices } from '../services/contact-services';
 
 @Component({
   selector: 'app-contact-form',
-  imports: [RouterModule],
+  imports: [RouterModule, ReactiveFormsModule],
   templateUrl: './contact-form.html',
   styleUrl: './contact-form.css'
 })
 export default class ContactForm {
+  private formBuilder = inject(FormBuilder);
+  private router = inject(Router);
+  private contactServices = inject(ContactServices);
 
+
+  form = this.formBuilder.group({
+    name: ['',Validators.required],
+    email: ['',Validators.required],
+  });
+
+  create(){
+    const contact = this.form.value;
+    this.contactServices.create(contact).subscribe(() => {
+      this.router.navigate(['/'])
+    });
+  }
 }
